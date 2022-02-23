080ca7efb56c5f8de4da2d0da94e04576d46127d
Eric Ye <yuxuan.smiley@gmail.com>
2022-02-23 10:33:05 -0800

    Use spaces for indentation

--- src/Banner.js
+++ src/Banner.js
@@ -1,20 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./Banner.css";
 
-function Banner(){
-	return(
-		<div className="Banner__banner">
-			<div className="Banner__logo">Logo</div>
-			<div className="Banner__spacer"></div>
-			<div className="Banner__navigation">Link1</div>
-			<div className="Banner__navigation">Link2</div>
-			<div className="Banner__navigation">Link3</div>
-			<div className="Banner__navigation">Link4</div>
-			<div className="Banner__navigation">Link5</div>
-			<div className="Banner__navigation">Link6</div>
-		</div>
-		)
+function Banner() {
+  return (
+    <div className="Banner__banner">
+      <div className="Banner__logo">Logo</div>
+      <div className="Banner__spacer"></div>
+      <div className="Banner__navigation">Link1</div>
+      <div className="Banner__navigation">Link2</div>
+      <div className="Banner__navigation">Link3</div>
+      <div className="Banner__navigation">Link4</div>
+      <div className="Banner__navigation">Link5</div>
+      <div className="Banner__navigation">Link6</div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
